46aa9559254140ce7613f9863202838c6b313dfa
Artur_A <arthursae@mail.ru>
2022-12-06 10:47:49 +0500

    Minor changes

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -7,11 +7,14 @@
 Console.Clear();
 Console.Write("Введите цифру, обозначающую день недели (1-7): ");
 int inputDay = int.Parse(Console.ReadLine());
-string weekDay = "не выходной день";
-string weekEnd = "выходной день!";
-string dash = " – ";
 
-string[] days = {"не является днем недели",
+string OutputDay(int day) {
+
+    string weekDay = "не выходной день";
+    string weekEnd = "выходной день!";
+    string dash = " – ";
+
+    string[] days = {"не является днем недели",
                         "понедельник",
                         "вторник", 
                         "среда", 
@@ -20,31 +23,36 @@ string[] days = {"не является днем недели",
                         "суббота", 
                         "воскресенье"};
 
-string OutputDay(int day) {
-
     switch(day) {
 
         case 1:
             return days[day] + dash + weekDay;
             break;
+
         case 2:
             return days[day] + dash + weekDay;
             break;
+
         case 3:
             return days[day] + dash + weekDay;
             break;
+
         case 4:
             return days[day] + dash + weekDay;
             break;
+
         case 5:
             return days[day] + dash + weekDay;
             break;
+
         case 6:
             return days[day] + dash + weekEnd; // weekend
             break;
+
         case 7:
             return days[day] + dash + weekEnd; // weekend
             break;
+
         default:
             return day + dash + days[0]; // exception
             break;
